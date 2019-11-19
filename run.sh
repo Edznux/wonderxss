3a5c3a46a8dea6eab367a545f45f802917b7727a
Edznux <edznux@gmail.com>
2019-11-20 00:59:45 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env sh
 
-run_with_doker()
-exit 0
+# Print executed commands & exit on first error
+set -ex;
 
+# global variables
 APP_NAME="wonderxss"
 
-chmod +x $APP_NAME
+run_with_docker(){
+    echo "Building docker container"
+    docker build -t edznux/$APP_NAME deploy/docker/
+    echo "Running docker container"
+    docker run -v $(pwd)/$APP_NAME.conf:/$APP_NAME.conf:ro -it edznux/$APP_NAME
+}
+
+run_with_docker
+
+exit 1
+
+
 # Add the capability to run on port 80 and 443 without root privileges.
 # Principle of least privilege. We don't need nor want root.
-sudo setcap 'cap_net_bind_service=+ep' $APP_NAME
-
-./$APP_NAME
+# sudo setcap 'cap_net_bind_service=+ep' $APP_NAME
 
 
-run_with_doker(){
-    # docker build -t edznux/wonderxss deploy/docker/
-    docker run -v wonderxss.conf:/wonderxss.conf -it edznux/wonderxss
-}
+
