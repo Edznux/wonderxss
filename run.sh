2fc07b488442e4c6abb9e705092d32237d43a990
Edznux <edznux@gmail.com>
2019-11-20 00:33:13 +0100

    Starts new run.sh to be able to launch every type of deployment

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+run_with_doker()
+exit 0
+
 APP_NAME="wonderxss"
 
 chmod +x $APP_NAME
@@ -8,3 +11,9 @@ chmod +x $APP_NAME
 sudo setcap 'cap_net_bind_service=+ep' $APP_NAME
 
 ./$APP_NAME
+
+
+run_with_doker(){
+    # docker build -t edznux/wonderxss deploy/docker/
+    docker run -v wonderxss.conf:/wonderxss.conf -it edznux/wonderxss
+}
