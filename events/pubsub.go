d7009a39e12b5428f1f8c62d8fe76de310750822
Edznux <edznux@gmail.com>
2019-11-27 00:33:59 +0100

    Add more events (def)

--- events/pubsub.go
+++ events/pubsub.go
@@ -4,11 +4,35 @@ import (
 	"github.com/cskr/pubsub"
 )
 
-const TOPIC_PAYLOAD_DELIVERED = "payload:delivered"
+const (
+	TOPIC_PAYLOAD_DELIVERED = "payload:delivered"
+	TOPIC_PAYLOAD_CREATED   = "payload:created"
+	TOPIC_PAYLOAD_DELETED   = "payload:deleted"
 
-var Events *pubsub.PubSub
+	TOPIC_ALIAS_CREATED = "alias:created"
+	TOPIC_ALIAS_DELETED = "alias:deleted"
+
+	TOPIC_USER_CREATED    = "user:created"
+	TOPIC_USER_CONNECT    = "user:connect"
+	TOPIC_USER_DISCONNECT = "user:disconnect"
+	TOPIC_USER_DELETE     = "user:delete"
+
+	TOPIC_LOOT_CREATED = "loot:created"
+)
+
+var (
+	TOPIC_ALL []string
+	Events    *pubsub.PubSub
+)
 
 func init() {
 	//create the new pubsub events queue
+	TOPIC_ALL = make([]string, 0)
+	TOPIC_ALL = append(TOPIC_ALL, TOPIC_PAYLOAD_DELIVERED)
+	TOPIC_ALL = append(TOPIC_ALL, TOPIC_PAYLOAD_CREATED)
+	TOPIC_ALL = append(TOPIC_ALL, TOPIC_PAYLOAD_DELETED)
+	TOPIC_ALL = append(TOPIC_ALL, TOPIC_ALIAS_CREATED)
+	TOPIC_ALL = append(TOPIC_ALL, TOPIC_ALIAS_DELETED)
+
 	Events = pubsub.New(0)
 }
