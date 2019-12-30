ad12d3395be62d960ecf73d3b90e22ac0de8001b
Edznux <edznux@gmail.com>
2019-12-30 15:24:06 +0100

    Fix payload & alias creation

--- ui/wonderxss/src/pages/PayloadEditor.js
+++ ui/wonderxss/src/pages/PayloadEditor.js
@@ -5,7 +5,7 @@ import { Container, Input, Button } from '@material-ui/core';
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-github";
 
-import { API_PAYLOADS } from '../helpers/constants';
+import { API_PAYLOADS, API_ALIASES } from '../helpers/constants';
 
 
 class PayloadEditor extends React.Component {
@@ -20,6 +20,17 @@ class PayloadEditor extends React.Component {
     };
     createAlias = (payload_id, alias) => {
         console.log(payload_id, alias)
+        if (!payload_id || !alias){
+            console.log("missing payload ID or alias name, not creating alias")
+            return
+        }
+        axios.post(API_ALIASES, {
+            alias: alias,
+            payload_id: payload_id
+        })
+        .then(res => {
+            console.log(res)
+        });
     };
     createPayload = () => {
         if (this.state.currentPayloadName === ""){
@@ -33,9 +44,9 @@ class PayloadEditor extends React.Component {
         })
         .then(res => {
             let payload_id = ""
-
-            if(res.data && !res.error){
-                payload_id = res.data.id
+            console.log(res.data.data)
+            if(res.data.data && !res.data.error){
+                payload_id = res.data.data.ID
                 if ( this.state.currentAlias !== ""){
                     this.createAlias(payload_id, this.state.currentAlias)
                 }
@@ -52,7 +63,8 @@ class PayloadEditor extends React.Component {
                     theme="github"
                     name="editor"
                     editorProps={{ $blockScrolling: true}}
-                    onChange={(value, event) => { this.setState({ "currentPayload": value }) }}
+                    onChange={(data) => {this.setState({ "currentPayload": data }) }}
+                    value={this.state.currentPayload}
                     />
                 <Input type="text" placeholder="Payload name" onChange={(event) => { this.setState({ "currentPayloadName": event.target.value }) }}></Input>
                 <Input type="text" placeholder="short-alias" onChange={(event) => { this.setState({ "currentAlias": event.target.value }) }}></Input>
