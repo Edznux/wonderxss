7566d0f3ce3a88554c839e09a8f0f8e937893187
Edznux <edznux@gmail.com>
2020-01-30 00:22:37 +0100

    Implement all get HTTP Client api

--- api/http/client/client.go
+++ api/http/client/client.go
@@ -141,20 +141,25 @@ func (c *Client) GetAliases() ([]api.Alias, error) {
 	if err != nil {
 		return []api.Alias{}, errors.New("Couldn't get Aliases: " + err.Error())
 	}
-	for _, a := range res.Data.([]interface{}) {
-		aliases = append(aliases, a.(api.Alias))
+	if res.Data != nil {
+		aliases = res.Data.([]api.Alias)
 	}
 	return aliases, nil
 }
 
 func (c *Client) GetAlias(id string) (api.Alias, error) {
 	var res api.Response
+	var alias api.Alias
 	res, err := c.doAPIRequest("GET", "/aliases/"+id, nil)
 	if err != nil {
 		return api.Alias{}, errors.New("Couldn't get Alias " + id + ": " + err.Error())
 	}
 
-	return res.Data.(api.Alias), nil
+	if res.Data != nil {
+		alias = res.Data.(api.Alias)
+	}
+
+	return alias, nil
 }
 
 func (c *Client) GetAliasByID(id string) (api.Alias, error) {
@@ -168,58 +173,159 @@ func (c *Client) GetAliasByPayloadID(id string) (api.Alias, error) {
 func (c *Client) AddAlias(name string, payloadId string) (api.Alias, error) {
 	return api.Alias{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) DeleteAlias(id string) error {
 	return errors.New("Not implemented yet")
 }
 
 func (c *Client) GetCollectors() ([]api.Collector, error) {
-	return []api.Collector{}, errors.New("Not implemented yet")
+	var res api.Response
+	var collectors []api.Collector
+	res, err := c.doAPIRequest("GET", "/collectors/", nil)
+	if err != nil {
+		return []api.Collector{}, errors.New("Couldn't get collectors " + err.Error())
+	}
+
+	if res.Data != nil {
+		collectors = res.Data.([]api.Collector)
+	}
+
+	return collectors, nil
 }
+
 func (c *Client) GetCollector(id string) (api.Collector, error) {
-	return api.Collector{}, errors.New("Not implemented yet")
+	var res api.Response
+	var collector api.Collector
+	res, err := c.doAPIRequest("GET", "/collectors/"+id, nil)
+	if err != nil {
+		return api.Collector{}, errors.New("Couldn't get Collector " + id + ": " + err.Error())
+	}
+
+	if res.Data != nil {
+		collector = res.Data.(api.Collector)
+	}
+
+	return collector, nil
 }
+
 func (c *Client) AddCollector(data string) (api.Collector, error) {
 	return api.Collector{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) DeleteCollector(id string) error {
 	return errors.New("Not implemented yet")
 }
 
 func (c *Client) GetExecutions() ([]api.Execution, error) {
-	return []api.Execution{}, errors.New("Not implemented yet")
+	var res api.Response
+	var executions []api.Execution
+	res, err := c.doAPIRequest("GET", "/executions/", nil)
+	if err != nil {
+		return []api.Execution{}, errors.New("Couldn't get executions " + err.Error())
+	}
+
+	if res.Data != nil {
+		executions = res.Data.([]api.Execution)
+	}
+
+	return executions, nil
 }
+
 func (c *Client) GetExecution(id string) (api.Execution, error) {
-	return api.Execution{}, errors.New("Not implemented yet")
+	var res api.Response
+	var execution api.Execution
+	res, err := c.doAPIRequest("GET", "/executions/"+id, nil)
+	if err != nil {
+		return api.Execution{}, errors.New("Couldn't get Execution " + id + ": " + err.Error())
+	}
+
+	if res.Data != nil {
+		execution = res.Data.(api.Execution)
+	}
+
+	return execution, nil
 }
+
 func (c *Client) AddExecution(payloadID string, aliasID string) (api.Execution, error) {
 	return api.Execution{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) DeleteExecution(id string) error {
 	return errors.New("Not implemented yet")
 }
 
 func (c *Client) GetInjections() ([]api.Injection, error) {
-	return []api.Injection{}, errors.New("Not implemented yet")
+	var res api.Response
+	var injections []api.Injection
+	res, err := c.doAPIRequest("GET", "/injections", nil)
+	if err != nil {
+		return []api.Injection{}, errors.New("Couldn't get Injections: " + err.Error())
+	}
+
+	if res.Data != nil {
+		injections = res.Data.([]api.Injection)
+	}
+
+	return injections, nil
 }
+
 func (c *Client) GetInjection(id string) (api.Injection, error) {
-	return api.Injection{}, errors.New("Not implemented yet")
+	var res api.Response
+	var injection api.Injection
+	res, err := c.doAPIRequest("GET", "/injections/"+id, nil)
+	if err != nil {
+		return api.Injection{}, errors.New("Couldn't get Injection " + id + ": " + err.Error())
+	}
+
+	if res.Data != nil {
+		injection = res.Data.(api.Injection)
+	}
+
+	return injection, nil
 }
+
 func (c *Client) AddInjection(name string, content string) (api.Injection, error) {
 	return api.Injection{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) DeleteInjection(id string) error {
 	return errors.New("Not implemented yet")
 }
 
 func (c *Client) GetPayloads() ([]api.Payload, error) {
-	return []api.Payload{}, errors.New("Not implemented yet")
+	var res api.Response
+	var payload []api.Payload
+	res, err := c.doAPIRequest("GET", "/payloads", nil)
+	if err != nil {
+		return []api.Payload{}, errors.New("Couldn't get Payload: " + err.Error())
+	}
+
+	if res.Data != nil {
+		payload = res.Data.([]api.Payload)
+	}
+
+	return payload, nil
 }
+
 func (c *Client) ServePayload(idOrAlias string) (string, error) {
 	return "", errors.New("Not implemented yet")
 }
+
 func (c *Client) GetPayload(id string) (api.Payload, error) {
-	return api.Payload{}, errors.New("Not implemented yet")
+	var res api.Response
+	var payload api.Payload
+	res, err := c.doAPIRequest("GET", "/payloads/"+id, nil)
+	if err != nil {
+		return api.Payload{}, errors.New("Couldn't get Payload " + id + ": " + err.Error())
+	}
+
+	if res.Data != nil {
+		payload = res.Data.(api.Payload)
+	}
+
+	return payload, nil
 }
+
 func (c *Client) AddPayload(name string, content string, contentType string) (api.Payload, error) {
 	return api.Payload{}, errors.New("Not implemented yet")
 }
@@ -231,15 +337,30 @@ func (c *Client) DeletePayload(id string) error {
 func (c *Client) GetUserByName(name string) (api.User, error) {
 	return api.User{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) GetUser(id string) (api.User, error) {
-	return api.User{}, errors.New("Not implemented yet")
+	var res api.Response
+	var user api.User
+	res, err := c.doAPIRequest("GET", "/users/"+id, nil)
+	if err != nil {
+		return api.User{}, errors.New("Couldn't get User " + id + ": " + err.Error())
+	}
+
+	if res.Data != nil {
+		user = res.Data.(api.User)
+	}
+
+	return user, nil
 }
+
 func (c *Client) CreateOTP(userID string, secret string, otp string) (api.User, error) {
 	return api.User{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) CreateUser(username, password string) (api.User, error) {
 	return api.User{}, errors.New("Not implemented yet")
 }
+
 func (c *Client) DeleteUser(id string) error {
 	return errors.New("Not implemented yet")
 }
